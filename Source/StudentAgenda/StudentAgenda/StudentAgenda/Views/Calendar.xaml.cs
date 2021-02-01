58c20ec0f1f320bcc2dcf66992ee012a44460ff0
RechetCoder <EiousDev7@gmail.com>
2021-02-01 12:53:36 -0500

    Should be error free, and have a calendar.

---
+++ Source/StudentAgenda/StudentAgenda/StudentAgenda/Views/Calendar.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using StudentAgenda.ViewModels;
+
+
+
+namespace StudentAgenda.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Calendar : ContentPage
+    {
+        public Calendar()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+
+        }
+    }
+}
